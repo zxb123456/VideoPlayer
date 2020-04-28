866aeba8ef81ed1adc26538f76ea4e4f5ad29682
zxb123456 <357687868@qq.com>
2020-04-28 10:03:27 +0800

    稍微修改

--- demo/src/main/java/cn/jzvd/demo/MainActivity.java
+++ demo/src/main/java/cn/jzvd/demo/MainActivity.java
@@ -2,6 +2,7 @@ package cn.jzvd.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         myJzvdStd.setUp("http://jzvd.nathen.cn/342a5f7ef6124a4a8faf00e738b8bee4/cf6d9db0bd4d41f59d09ea0a81e918fd-5287d2089db37e62345123a1be272f8b.mp4"
                 , "饺子快长大");
         Glide.with(this).load("http://jzvd-pic.nathen.cn/jzvd-pic/1bb2ebbe-140d-4e2e-abd2-9e7e564f71ac.png").into(myJzvdStd.posterImageView);
-
+        String test = "hhhhhhhhhhhhhhhhh";
     }
 
     @Override
